c911e128c2f735f15e431249d17db399d9758b46
airintania19-commits <airintania19@gmail.com>
2025-10-01 21:24:51 +0700

    pair 2
    selesai

--- T00.java
+++ T00.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class T00 {
+public class JavaApplication {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
